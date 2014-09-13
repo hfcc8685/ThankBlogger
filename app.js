cdc01cdc278b11d377ed51ab4664b3bbde002977
hfcc8685 <hfcc8685@gmail.com>
2014-09-13 21:56:08 +0800

    添加co-views,koa-router模块

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var koa = require('koa');
-var app = koa();
+var koa = require('koa')
+  , render = require('./lib/render')
+  , router = require('koa-router')
+  , app = koa();
 
-app.use(function *(){
-	this.body = 'hello World';
-});
+app.use(router(app));
+
+app.get('/TimeLine', timeLine);
+app.get('/AboutUs', aboutUs);
+
+function *timeLine() {
+	this.body = yield render('timeLine');
+}
+
+function *aboutUs() {
+	this.body = "aboutUs";
+}
 
 app.listen(3000);
