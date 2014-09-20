2c16cf72082bb2cffda1c8d98e495a4783e3d2b3
hfcc8685 <hfcc8685@gmail.com>
2014-09-20 15:22:07 +0800

    home

--- index.js
+++ index.js
@@ -11,12 +11,84 @@ app.use(staticServer(path.join(__dirname, 'assets')));
 
 app.use(router(app));
 
-
+app.get('/',home);
+app.get('/Home',home);
+app.get('/Blogger/:id',blogger)
 app.get('/TimeLine', timeLine);
 app.get('/AboutUs', aboutUs);
-app.get('/Home',home);
+
 
 var blogs = [
+{
+	id:001,
+	name:'hanfeng',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'He is a 2B youth!',
+    hearts:10000
+},
+{
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!',
+    hearts:12000
+},
+{
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql','ruby','音乐','美术','文学','美学','mysql','ruby','音乐','美术'],
+    evaluation:'I am cuican!',
+    hearts:12000
+},
+{
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!',
+    hearts:12000
+},
+{
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!I am cuican!',
+    hearts:12000
+},
+{
+
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!',
+    hearts:12000
+
+},
+{
+
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!',
+    hearts:12000
+
+},
+{
+
+	id:002,
+	name:'cuican',
+	blogUri:"http://localhost:3000/AboutUs",
+    tags:['java','c#','javascript','mysql'],
+    evaluation:'I am cuican!',
+    hearts:12000
+
+},
 {
 	id:001,
 	name:'hanfeng',
@@ -90,15 +162,20 @@ var blogs = [
 ];
 
 
+function *home(){
+	this.body = yield render('home',{blogs:blogs});
+}
+
+function *blogger() {
+	this.body = this.params.id;
+}
+
 function *timeLine() {
 	this.body = yield render('timeline');
 }
 
 function *aboutUs() {
 	this.body = yield render('aboutUs');
-}
-function *home(){
-	this.body = yield render('home',{blogs:blogs});
 }
 
 app.listen(3000);
